bd8827997ef41dd2550bf196b322c0132d09c5e7
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2024-03-09 11:01:31 +0200

    Main function and empty raytracer

---
+++ IO/writer.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <fstream>
+#include "writer.h"
+
+void writePPM(SceneImage &sceneImage, const char *path) {
+    // write ppm file
+}
---
+++ IO/writer.h
@@ -0,0 +1,8 @@
+#ifndef PROJECT_WRITER_H
+#define PROJECT_WRITER_H
+
+#include "../Structures/SceneImage.h"
+
+void writePPM(SceneImage&, const char*);
+
+#endif //PROJECT_WRITER_H
