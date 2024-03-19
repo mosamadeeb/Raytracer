11739d4e9dbe25169bf4a92701c8e90d39995aa9
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2024-03-19 07:59:01 +0200

    Implement PPM writer

--- IO/writer.cpp
+++ IO/writer.cpp
@@ -3,5 +3,20 @@
 #include "writer.h"
 
 void writePPM(SceneImage &sceneImage, const char *path) {
-    // write ppm file
+    std::ofstream output(path);
+
+    output << "P6\n";       // magic
+    output << sceneImage.width << ' ' << sceneImage.height << '\n';
+    output << 255 << '\n';  // max color value
+
+    // height rows, each consisting of width pixels, each pixel is rgb
+    for (int i = 0; i < sceneImage.height; i++) {
+        for (int j = 0; j < sceneImage.width * 3; j += 3) {
+            output << sceneImage.buffer[i][j]
+                << sceneImage.buffer[i][j + 1]
+                << sceneImage.buffer[i][j + 2];
+        }
+    }
+
+    output.close();
 }
